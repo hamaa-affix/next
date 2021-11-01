8e0792dbd7ec36434b8c1407e1c5721ec47acb00
hamaa-affix <hamaagive1108@gmail.com>
2021-11-02 00:39:27 +0900

    map

--- compoents/Links.jsx
+++ compoents/Links.jsx
@@ -1,34 +1,37 @@
 // @ts-nocheck
 export const Links = () => {
+    const ITEMS = [
+        {
+            href: "https://nextjs.org/docs",
+            title: "Documentation &rarr;",
+            description: "Find in-depth information about Next.js features and API."
+        },
+        {
+            href: "https://nextjs.org/learn",
+            title: "Documentation &rarr;",
+            description: "Find in-depth information about Next.js features and API."
+        },
+        {
+            href: "https://github.com/vercel/next.js/tree/master/examples",
+            title: "Documentation &rarr;",
+            description: "Discover and deploy boilerplate example Next.js projects."
+        },
+        {
+            href: "https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+            title: "Deploy &rarr;",
+            description: "Instantly deploy your Next.js site to a public URL with Vercel."
+        },
+    ]
     return(
       <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/vercel/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-         </a>
+        { ITEMS.map( item => {
+          return (
+            <a key={key.href} href={ item.href } className="card">
+              <h3>{ item.title }</h3>
+              <p>{ item.description }</p>
+            </a>
+          )
+        })}
     </div>
     );
   }
