cbc3b0f1d295d3f0c687a2076c69e6a2c4386190
hamaa-affix <hamaagive1108@gmail.com>
2021-11-08 10:36:53 +0900

    useReducer

--- compoents/posts/index.jsx
+++ compoents/posts/index.jsx
@@ -1,10 +1,37 @@
 // @ts-nocheck
-import { useState, useCallback, useEffect } from "react";
+import { useCallback, useEffect, useReducer } from "react";
+
+const initialState = {
+    data: [],
+    loading: true,
+    error: null
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "end":
+      return {
+        ...state,
+        data: action.data,
+        loading: false
+      }
+    case "error":
+      return {
+        ...state,
+        loading: false,
+        error: action.error
+      }
+    default:
+      throw new Error("no such action type!")
+  }
+}
 
 export const Posts = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [posts, setPosts] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
+  // const [posts, setPosts] = useState([]);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const getPosts = useCallback(async () => {
     try {
@@ -13,22 +40,28 @@ export const Posts = () => {
       if(!res.ok) {
         throw new Error("エラーが発生した為、データの取得に失敗しました");
       }
-      setPosts(json);
+      dispatch({
+        type: "end",
+        data: json
+      })
     } catch(error) {
-      setError(error);
+      dispatch({
+        type: "error",
+        error: error
+      });
     }
-    setIsLoading(false);
+    //setIsLoading(false);
   }, []);
 
   useEffect(() => {
       getPosts();
   }, [getPosts])
 
-  if(isLoading) {
+  if(state.loading) {
     return <div>ローディング中です</div>
   }
 
-  if(error) {
+  if(state.error) {
     return <div>{error.message}</div>
   }
 
@@ -37,7 +70,7 @@ export const Posts = () => {
   }
   return (
     <ol>
-      {posts.map(post => {
+      {state.data.map(post => {
         return (
           <li key={post.id}>{post.title}</li>
         );
